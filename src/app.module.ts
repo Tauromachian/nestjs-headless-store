696c5158b77c333562596d72dad374c90c8c9a21
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-04 16:39:15 -0400

    Throttle requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,28 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     ItemsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 60,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}

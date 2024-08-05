513d74ec16559894d960e0d9fb89de32ab48412f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-05 19:59:36 -0400

    Add validation logic

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNumber, IsString } from 'class-validator';
+
+import { Role } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsEnum(Role)
+  role: Role;
+}

4f1b831beede23278da8cf3f0a4cdf2393748202
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-06 09:35:15 -0400

    Remove tag

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);

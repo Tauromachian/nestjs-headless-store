174d57b078da769e217e0154fc96d5de2061d173
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-06 14:01:46 -0400

    Add swagger specification

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,25 @@
 import { IsEmail, IsEnum, IsNumber, IsString } from 'class-validator';
 
 import { Role } from '../entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsNumber()
   id: number;
 
   @IsString()
+  @ApiProperty({ example: 'Jose', description: 'Name of the user' })
   name: string;
 
   @IsEmail()
+  @ApiProperty({ example: 'email@asdf.com', description: 'Email of the user' })
   email: string;
 
   @IsString()
+  @ApiProperty({ example: 'asdf1234', description: 'Password of the user' })
   password: string;
 
   @IsEnum(Role)
+  @ApiProperty({ example: Role.CUSTOMER, description: 'Role of the user' })
   role: Role;
 }

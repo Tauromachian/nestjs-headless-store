0f164b2c6530a71e75e558bf85ceae957e6b8d56
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-11 15:20:24 -0400

    Inject TypeOrmModule

--- src/cart-items/cart-items.module.ts
+++ src/cart-items/cart-items.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CartItemsService } from './cart-items.service';
 import { CartItemsController } from './cart-items.controller';
 
+import { CartItem } from './entities/cart-item.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([CartItem])],
   controllers: [CartItemsController],
   providers: [CartItemsService],
 })

1dcf511c1c538de560acb284c03c6bdf7b3e4431
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-11 16:05:16 -0400

    Add cart item dto

--- src/cart-items/dto/create-cart-item.dto.ts
+++ src/cart-items/dto/create-cart-item.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCartItemDto {}
+import { IsNumber } from 'class-validator';
+
+import { Cart } from 'src/carts/entities/cart.entity';
+import { Item } from 'src/items/entities/item.entity';
+
+import { IsInDatabase } from 'src/custom-validators/IsInDatabase';
+
+export class CreateCartItemDto {
+  @IsNumber()
+  quantity: number;
+
+  @IsInDatabase(Cart, 'id')
+  cartId: string;
+
+  @IsInDatabase(Item, 'id')
+  itemId: string;
+}

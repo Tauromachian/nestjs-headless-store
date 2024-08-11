85cdd6470f6835b1c7702be28dc6a46021bcd2a7
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-11 15:07:45 -0400

    Make custom validators IsInDatabase class

---
+++ src/custom-validators/IsInDatabase.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+import { EntityManager } from 'typeorm';
+
+@ValidatorConstraint({ async: true })
+export class IsInDatabaseConstraint implements ValidatorConstraintInterface {
+  constructor(private readonly entityManager: EntityManager) {}
+
+  async validate(value: string, args: ValidationArguments) {
+    const [EntityClass, property = 'id'] = args.constraints;
+
+    const repository = this.entityManager.getRepository(EntityClass);
+
+    const row = await repository.findOne({
+      where: { [property]: value },
+    });
+
+    return !!row;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `${args.property} does not exist in database`;
+  }
+}
+
+export function IsInDatabase<G>(
+  entity: G,
+  property?: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [entity, property],
+      validator: IsInDatabaseConstraint,
+    });
+  };
+}

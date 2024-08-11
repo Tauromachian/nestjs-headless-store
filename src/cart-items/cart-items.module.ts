800ee3d95d66271daa3b21569643fc9a5f8e0151
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-11 15:21:06 -0400

    Inject IsInDatabaseConstraint

--- src/cart-items/cart-items.module.ts
+++ src/cart-items/cart-items.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartItemsService } from './cart-items.service';
 import { CartItemsController } from './cart-items.controller';
 
+import { IsInDatabaseConstraint } from 'src/custom-validators/IsInDatabase';
+
 import { CartItem } from './entities/cart-item.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CartItem])],
   controllers: [CartItemsController],
-  providers: [CartItemsService],
+  providers: [CartItemsService, IsInDatabaseConstraint],
 })
 export class CartItemsModule {}

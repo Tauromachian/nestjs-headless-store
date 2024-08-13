73e6debff01ec596bbd80e93b8ae9781d417cd34
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-13 11:42:40 -0400

    Use PartiType from @nestjs/swagger

--- src/carts/dto/update-cart.dto.ts
+++ src/carts/dto/update-cart.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCartDto } from './create-cart.dto';
 
 export class UpdateCartDto extends PartialType(CreateCartDto) {}

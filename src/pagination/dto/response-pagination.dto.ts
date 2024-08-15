d4c40161ac9c3088eed3ef8604a7a33174b779c5
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-15 08:51:01 -0400

    Add swagger declarations for the response pagination

--- src/pagination/dto/response-pagination.dto.ts
+++ src/pagination/dto/response-pagination.dto.ts
@@ -1,21 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class ResponsePaginationDto<G> {
+  @ApiProperty({
+    description: 'The total number of items available.',
+    example: 100,
+  })
   @IsNumber()
   total: number;
 
+  @ApiProperty({
+    description: 'The number of items per page.',
+    example: 10,
+  })
   @IsNumber()
   per_page: number;
 
+  @ApiProperty({
+    description: 'The current page number.',
+    example: 1,
+  })
   @IsNumber()
   current_page: number;
 
+  @ApiProperty({
+    description: 'The starting index of the current page items.',
+    example: 1,
+  })
   @IsNumber()
   from: number;
 
+  @ApiProperty({
+    description: 'The ending index of the current page items.',
+    example: 10,
+  })
   @IsNumber()
   to: number;
 
-  @IsNumber()
+  @ApiProperty({
+    description: 'The data items for the current page.',
+    isArray: true,
+  })
   data: G[];
 }

7dfdfd5106b2529d4c410798573af6995af8c33a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-15 16:41:34 -0400

    Mark login and register as public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,21 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   login(@Body() signInDto: Record<string, string>) {
     return this.authService.login(signInDto.email, signInDto.password);
   }
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('register')
   register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);

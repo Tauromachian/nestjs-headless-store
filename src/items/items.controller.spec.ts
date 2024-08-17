19744b199aca776b492e98f72910469dd5497e9f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-17 12:01:43 -0400

    Add simple findAll test

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -4,6 +4,9 @@ import { ItemsService } from './items.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Item } from './entities/item.entity';
 import { Repository } from 'typeorm';
+import { ResponsePaginationDto } from 'src/pagination/dto/response-pagination.dto';
+import { ResponseItemDto } from './dto/response-item.dto';
+import { QueryPaginationDto } from 'src/pagination/dto/query-pagination.dto';
 
 describe('ItemsController', () => {
   let controller: ItemsController;
@@ -24,5 +27,38 @@ describe('ItemsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('Should return and array of paginated items', async () => {
+      const paginationRequest: QueryPaginationDto = {
+        limit: 15,
+        page: 1,
+      };
+
+      const result: ResponsePaginationDto<ResponseItemDto> = {
+        total: 2,
+        per_page: 10,
+        current_page: 1,
+        from: 1,
+        to: 10,
+        data: [
+          {
+            id: 1,
+            name: 'test1',
+            price: 10,
+            quantity: 10,
+            currency: 'USD',
+            description: 'test1',
+          },
+        ],
+      };
+
+      jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+      console.log(await controller.findAll(paginationRequest));
+
+      expect(await controller.findAll({})).toBe(result);
+    });
   });
 });

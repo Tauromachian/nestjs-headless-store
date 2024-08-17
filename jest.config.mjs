622df1fa50dad64638ccf10d23b3301dde4567c1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-17 11:46:58 -0400

    Set dedicated file for jest

---
+++ jest.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+};

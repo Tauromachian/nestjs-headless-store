5eb4b2a7b38bb4bc406d18263f3832fec451e2b1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-17 11:47:22 -0400

    Mock repositories

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
 
 describe('ItemsController', () => {
   let controller: ItemsController;
@@ -8,7 +11,10 @@ describe('ItemsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        { provide: getRepositoryToken(Item), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +11,10 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useClass: Repository },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);

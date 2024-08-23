ff5430ad016e2b5812d95fed8e8eaaef65a6fb96
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-23 13:16:33 -0400

    Add swagger declarations

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,33 +1,86 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+
 import { NotificationsService } from './notifications.service';
+import { Filter } from 'src/filters/decorators/filter.decorator';
+
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { QueryFilterDto } from 'src/filters/dto/query-filters.dto';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new notification' })
+  @ApiResponse({
+    status: 201,
+    description: 'The notification has been successfully created.',
+    type: CreateNotificationDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   create(@Body() createNotificationDto: CreateNotificationDto) {
     return this.notificationsService.create(createNotificationDto);
   }
 
   @Get()
-  findAll() {
-    return this.notificationsService.findAll();
+  @ApiOperation({ summary: 'Retrieve all notifications' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of notifications',
+    type: [CreateNotificationDto],
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  findAll(@Filter() filterDto: QueryFilterDto) {
+    return this.notificationsService.findAll(filterDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a single notification by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the notification to retrieve' })
+  @ApiResponse({
+    status: 200,
+    description: 'The notification details',
+    type: CreateNotificationDto,
+  })
+  @ApiResponse({ status: 404, description: 'Notification not found' })
   findOne(@Param('id') id: string) {
     return this.notificationsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNotificationDto: UpdateNotificationDto) {
+  @ApiOperation({ summary: 'Update an existing notification by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the notification to update' })
+  @ApiResponse({
+    status: 200,
+    description: 'The updated notification',
+    type: UpdateNotificationDto,
+  })
+  @ApiResponse({ status: 404, description: 'Notification not found' })
+  update(
+    @Param('id') id: string,
+    @Body() updateNotificationDto: UpdateNotificationDto,
+  ) {
     return this.notificationsService.update(+id, updateNotificationDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a notification by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the notification to delete' })
+  @ApiResponse({
+    status: 200,
+    description: 'The notification has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Notification not found' })
   remove(@Param('id') id: string) {
     return this.notificationsService.remove(+id);
   }

8b9757090d433bf89479f4e2f766d4e995ad5b55
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-23 09:12:15 -0400

    Make basic notification entity

--- src/notifications/entities/notification.entity.ts
+++ src/notifications/entities/notification.entity.ts
@@ -1,1 +1,38 @@
-export class Notification {}
+import { Base } from 'src/shared/entities/base.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+
+export enum NotificationType {
+  EMAIL = 'email',
+  SMS = 'sms',
+  IN_APP = 'in-app',
+}
+
+@Entity()
+export class Notification extends Base {
+  @Column()
+  title: string;
+
+  @Column()
+  text: string;
+
+  @Column({
+    type: 'enum',
+    enum: NotificationType,
+    default: NotificationType.IN_APP,
+  })
+  type: string; // e.g., 'email', 'sms', 'in-app'
+
+  @Column({ default: false })
+  isSent: boolean;
+
+  @Column({ default: false })
+  isRead: boolean;
+
+  @ManyToOne(() => User, (user: User) => user.notifications)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: number;
+}

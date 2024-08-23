bd48995c0ce1fe9dbf168bb088f164325b8fb735
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-23 11:21:58 -0400

    Add simple logic to the service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { filter } from 'src/filters/helpers';
+
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { ResponseNotificationDto } from './dto/response-notification.dto';
+import { QueryFilterDto } from 'src/filters/dto/query-filters.dto';
+import { ResponseFilterDto } from 'src/filters/dto/response-filters.dto';
+
+import { Notification } from './entities/notification.entity';
 
 @Injectable()
 export class NotificationsService {
-  create(createNotificationDto: CreateNotificationDto) {
-    return 'This action adds a new notification';
+  constructor(
+    @InjectRepository(Notification)
+    private readonly notificationsRepository: Repository<Notification>,
+  ) {}
+
+  create(
+    createNotificationDto: CreateNotificationDto,
+  ): Promise<ResponseNotificationDto> {
+    const notification = this.notificationsRepository.create(
+      createNotificationDto,
+    );
+
+    const newNotification = this.notificationsRepository.save(notification);
+
+    return newNotification;
   }
 
-  findAll() {
-    return `This action returns all notifications`;
+  async findAll(
+    paginationDto: QueryFilterDto,
+  ): Promise<ResponseFilterDto<Partial<ResponseNotificationDto>>> {
+    return filter(this.notificationsRepository, paginationDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} notification`;
+  async findOne(id: number): Promise<ResponseNotificationDto> {
+    const notification = await this.notificationsRepository.findOne({
+      where: { id },
+    });
+
+    if (!notification) throw new NotFoundException();
+
+    return notification;
   }
 
-  update(id: number, updateNotificationDto: UpdateNotificationDto) {
-    return `This action updates a #${id} notification`;
+  async update(
+    id: number,
+    updateNotificationDto: UpdateNotificationDto,
+  ): Promise<ResponseNotificationDto> {
+    const updateResult = await this.notificationsRepository.update(
+      id,
+      updateNotificationDto,
+    );
+
+    if (updateResult.affected === 0) throw new NotFoundException();
+
+    return this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} notification`;
+  async remove(id: number): Promise<{ message: string }> {
+    const result = await this.notificationsRepository.delete({ id });
+
+    if (result.affected === 0) throw new NotFoundException();
+
+    return { message: 'Notification deleted successfully' };
   }
 }

48414d309bebe12d337f3d2f179a9788cb376500
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-23 09:31:57 -0400

    Make basic create notification dto

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,60 @@
-export class CreateNotificationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+import { NotificationType } from '../entities/notification.entity';
+
+export class CreateNotificationDto {
+  @ApiProperty({
+    description: 'The title of the notification',
+    example: 'Welcome to our service!',
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    description: 'The text content of the notification',
+    example: 'Your account has been successfully created.',
+  })
+  @IsString()
+  @IsNotEmpty()
+  text: string;
+
+  @ApiProperty({
+    description: 'The type of notification',
+    enum: NotificationType,
+    example: NotificationType.EMAIL,
+    default: NotificationType.IN_APP,
+  })
+  @IsEnum(NotificationType)
+  type: NotificationType;
+
+  @ApiProperty({
+    description: 'Indicates if the notification has been sent',
+    example: false,
+    default: false,
+  })
+  @IsBoolean()
+  isSent: boolean;
+
+  @ApiProperty({
+    description: 'Indicates if the notification has been read by the user',
+    example: false,
+    default: false,
+  })
+  @IsBoolean()
+  isRead: boolean;
+
+  @ApiProperty({
+    description: 'The ID of the user receiving the notification',
+    example: 1,
+  })
+  @IsNumber()
+  userId: number;
+}

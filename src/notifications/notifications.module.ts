b02abe74947f201beed9661655656ffe69dbd756
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-25 11:35:00 -0400

    Export notifications service

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -9,5 +9,6 @@ import { Notification } from './entities/notification.entity';
   imports: [TypeOrmModule.forFeature([Notification])],
   controllers: [NotificationsController],
   providers: [NotificationsService],
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}

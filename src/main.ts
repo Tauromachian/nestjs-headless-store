86e7560b492417403e990f39fd5ec2515e552123
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-08-28 15:20:10 -0400

    Add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
 
